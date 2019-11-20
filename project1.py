033aa94f131f4c85536db687347e3401d51f69df
ethan21-meet <52971614+ethan21-meet@users.noreply.github.com>
2019-11-20 13:03:44 -0500

    Add files via upload

--- project1.py
+++ project1.py
@@ -1,33 +1,72 @@
+users=[]
+Posts=[]
+
 class User(object):
-	def __init__(self,name,email,password,friends_list,posts):
+	def __init__(self,name,email,password,):
+		
+		
 		self.name = name
 		self.email = email
 		self.password = password
-		self.friends_list = []
-		self.post = []
+		self.friends_list =[]
+		
+
+
 
 	def add_friends (self,email):
-		friends_list.append(email)
-		print(self.name + "has added" + email + "as a friend")
+		self.friends_list.append(email)
+		print(self.name + " has added " + email + " as your friend")
 
 	def remove_friend (self,email):
-		friends_list.remove(email)
-		print( self.name + "has removed" + email + "as a friend")
+		self.friends_list.remove(email)
+		print( self.name + " has removed " + email + " as a friend")
+	
 
 	def post(self,text):
-
-		post.append(text)
-		print(self.name + "has posted" + text)
+		posst = Post(text, 0, 0, self.email)
+		posts.append(posst)
+		print(self.name + " has posted " + text)
 
 	def get_userInfo(self):
-		print("Name: ["+self.name+"] "+ "Email:["+self.email+"] "+ "Password:["+self.password+"] "+ "Friends:["+str(self.friends_list)+"])"+"Posts:["+str(self.post)+"]")
+		print("Name: ["+self.name+"] "+ "Email:["+self.email+"] "+ "Password:["+self.password+"] "+ "Friends:["+str(self.friends_list))
 
 
-user1 = User("josh","joshi8@meet.mit.edu","helloooo")
-user2 = User("andres","andresini@meet.mit.edu","liverpool")
-user1.add_friends("andresini@meet.mit.edu")
-user1.post("i love my best friends")
+user1= User("josh" , "joshi8@meet.mit.edu" ,"helloooo"  )
+user2 = User("andres" ,"andresini@meet.mit.edu" ,"liverpool" )
+
 
 user1.get_userInfo()
-user2.get_userInfo()
-user1.remove_friend("andresini@meet.mit.edu")
+
+class post(object):
+	def __init__(self,text,num_likes,num_comments,author):
+		self.num_likes = 0
+		self.num_comments = 0
+		self.text = text
+		self.author = author
+	
+	def add_likes(self):
+		self.num_likes = self.num_likes + 1
+
+		print("i liked this post!!")
+
+	def add_comments(self):
+		self.num_comments = self.num_comments + 1
+user1.post("hi hello")
+user1.get_userInfo()
+
+
+
+
+
+
+
+		
+
+
+	 
+
+
+
+
+
+
